6fab084c2bd9dd1772abfd09ba6a56c38801c958
何啟賢 <qaz326978547@gmail.com>
2023-03-20 17:14:55 +0800

    deploy

---
+++ assets/alertStore-0e49609d.js
@@ -0,0 +1,1 @@
+import{B as r,J as a}from"./index-a017f003.js";const o=r("alert",{state:()=>({}),actions:{getState(t,e){a({title:t,icon:e?"success":"error",timer:2e3})}}});export{o as a};
--- assets/alertStore-ac2ff4cb.js
+++
@@ -1,1 +0,0 @@
-import{B as r,J as a}from"./index-e6ea0b44.js";const o=r("alert",{state:()=>({}),actions:{getState(t,e){a({title:t,icon:e?"success":"error",timer:2e3})}}});export{o as a};
